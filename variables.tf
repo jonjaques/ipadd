5abe66806594510cf595fb2f0ad470ca42671ac6
Jon Jaques <jaquers@gmail.com>
2020-11-06 21:31:00 -0600

    Add some helpful outputs

--- variables.tf
+++ variables.tf
@@ -19,3 +19,15 @@ variable dest_dir { default = "/home/coder" }
 locals {
   key_name = "${var.name}-deployment"
 }
+
+output "public_ip" {
+  value = aws_instance.ipadd.public_ip
+}
+
+output "key_pem" {
+  value = "${tls_private_key.deployer.private_key_pem}${tls_private_key.deployer.public_key_pem}"
+}
+
+output "ssh_access" {
+  value = "ssh -i ~/.ssh/ipadd.pem arch@${aws_instance.ipadd.public_ip}"
+}
