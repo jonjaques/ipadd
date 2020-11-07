e83322fe48b73065d18821a4ba33b8159f619fd3
Jon Jaques <jaquers@gmail.com>
2020-11-07 00:26:50 -0600

    Touch up

--- variables.tf
+++ variables.tf
@@ -23,7 +23,8 @@ variable src_dir { default = "code" }
 variable dest_dir { default = "/home/coder" }
 
 locals {
-  key_name = "${var.name}-deployment"
+  key_name     = "${var.name}-deployment"
+  key_contents = "${tls_private_key.deployer.private_key_pem}${tls_private_key.deployer.public_key_pem}"
 }
 
 output "public_ip" {
@@ -31,7 +32,11 @@ output "public_ip" {
 }
 
 output "key_pem" {
-  value = "${tls_private_key.deployer.private_key_pem}${tls_private_key.deployer.public_key_pem}"
+  value = local.key_contents
+}
+
+output "save_key" {
+  value = "rm -f ~/.ssh/ipadd.pem && echo \"${jsonencode(local.key_contents)}\" > ~/.ssh/ipadd.pem && chmod 400 ~/.ssh/ipadd.pem"
 }
 
 output "ssh_access" {
